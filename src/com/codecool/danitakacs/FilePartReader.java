f8817cdd0c09b8bb265ac7268145a3668b7e39f3
Dániel Takács <takacs.d24@gmail.com>
2020-03-11 13:20:20 +0100

    Finished with the core application

---
+++ src/com/codecool/danitakacs/FilePartReader.java
@@ -0,0 +1,54 @@
+package com.codecool.danitakacs;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FilePartReader {
+
+    private String filePath;
+    private Integer fromLine;
+    private Integer toLine;
+
+    FilePartReader() {
+        filePath = "";
+        fromLine = 0;
+        toLine = -1;
+    }
+
+    public void setup(String filePath, Integer fromLine, Integer toLine) {
+        if (toLine < fromLine || fromLine < 1) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
+    }
+
+    public String read() throws IOException {
+        File file = new File(filePath);
+        Scanner fileToBeRead = new Scanner(file);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (fileToBeRead.hasNext()) {
+            String line = fileToBeRead.nextLine();
+            stringBuilder.append(line);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString().trim();
+
+    }
+
+    public String readLines () throws IOException {
+        String fileContent = read();
+        String[] lines = fileContent.split("\n");
+        StringBuilder returnContent = new StringBuilder();
+        for (int i = 0; i < lines.length; i++){
+            if (i+1 >= fromLine && i+1 <= toLine){
+                returnContent.append(lines[i]);
+                returnContent.append(" ");
+            }
+        }
+        return returnContent.toString().trim();
+    }
+
+}
---
+++ src/com/codecool/danitakacs/FileWordAnalyzer.java
@@ -0,0 +1,66 @@
+package com.codecool.danitakacs;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileWordAnalyzer {
+
+    FilePartReader filePartReader;
+
+    FileWordAnalyzer(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+    }
+
+    public List getWordsOrderedAlphabetically() throws IOException {
+        List<String> words = turnStringIntoListOfWords();
+        java.util.Collections.sort(words, String.CASE_INSENSITIVE_ORDER);
+        return words;
+    }
+
+
+    public List getWordsContainingSubstring(String subString) throws IOException {
+        if (subString.isEmpty()) {
+            return null;
+        }
+        List<String> words = turnStringIntoListOfWords();
+        List<String> wordsContainingSubstring = new ArrayList<>();
+        for (String word : words) {
+            if (word.toLowerCase().contains(subString.toLowerCase())) {
+                wordsContainingSubstring.add(word);
+            }
+        }
+        return wordsContainingSubstring;
+    }
+
+    public List getStringsWhichPalindromes() throws IOException {
+        List<String> words = turnStringIntoListOfWords();
+        List<String> palindromeWords = new ArrayList<>();
+        for (String word : words) {
+            if (checkIfPalindrome(word)) {
+                palindromeWords.add(word);
+            }
+        }
+        return palindromeWords;
+    }
+
+    private List<String> turnStringIntoListOfWords() throws IOException {
+        String text = filePartReader.readLines();
+        return Arrays.asList(text.split(" "));
+    }
+
+    private boolean checkIfPalindrome(String word) {
+        word = word.toLowerCase();
+        int i1 = 0;
+        int i2 = word.length() - 1;
+        while (i2 > i1) {
+            if (word.charAt(i1) != word.charAt(i2)) {
+                return false;
+            }
+            i1++;
+            i2--;
+        }
+        return true;
+    }
+
+}
